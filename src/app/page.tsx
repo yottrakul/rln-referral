7225fe714b3051a6bde299f322bc93d2b57dfb31
Thanapat Rattanamontree <thanapat16849@gmail.com>
2024-01-22 22:16:27 +0700

    chore: add header3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <main>
       <div className="header-banner">Refferal system</div>
+      <h1>Hello world test add 2</h1>
       Hello RLN Referral system Test Github Hook
       <button>Hello Fighter</button>
+      
     </main>
   );
 }
