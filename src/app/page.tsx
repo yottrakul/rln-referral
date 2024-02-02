014557317a7375767697f785157cfac6fee51171
Yottrakul Suwannasri <yottrakul_s.vo@tripetch-it.co.th>
2024-02-02 15:27:59 +0700

    fix: intregate chakra provide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@chakra-ui/react";
+
 export default function Home() {
   return (
     <main>
@@ -5,7 +7,7 @@ export default function Home() {
       <h1>Hello world test add 2</h1>
       Hello RLN Referral system Test Github Hook
       <button>Hello Fighter</button>
-      
+      <Button colorScheme="blue">Button</Button>
     </main>
   );
 }
