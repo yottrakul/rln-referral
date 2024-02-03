acd469ef7fc15ace84aacdd85a89ab3e03f25ff1
Yottrakul Suwannasri <yottrakul_s.vo@tripetch-it.co.th>
2024-02-03 13:18:57 +0700

    chore: test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Center minHeight={"100vh"}>
         <Container maxW={"md"}>
           <LoginForm />
+          Test
         </Container>
       </Center>
     </main>
