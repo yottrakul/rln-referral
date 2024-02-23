00ab8955e18a244e344161e5bbe71027a6b412bb
Yottrakul Suwannasri <yottrakul_s.vo@tripetch-it.co.th>
2024-02-24 01:44:37 +0700

    fix: move test route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,3 @@
-import RegisterForm from "@/app/_components/ui/Register/RegisterForm";
-import LogoutButton from "@/app/_components/ui/Logout/LogoutButton";
-import UserDetailsClientComponent from "../_components/ui/Example/UserAuthDetails/ClientComponent";
-import UserDetailsServerComponent from "../_components/ui/Example/UserAuthDetails/ServerComponent";
-import SkeletonUserDetail from "../_components/ui/Example/Skeleton/UserDetails";
-import { Suspense } from "react";
-// import { auth } from "@/server/auth";
-
 export default function Dashboard() {
-  return (
-    <main>
-      <UserDetailsClientComponent />
-      <Suspense fallback={<SkeletonUserDetail />}>
-        <UserDetailsServerComponent />
-      </Suspense>
-      <RegisterForm />
-      <LogoutButton />
-    </main>
-  );
+  return <main>dashboard</main>;
 }
--- src/app/test/dashboard/page.tsx
+++ src/app/test/dashboard/page.tsx
@@ -1,7 +1,20 @@
+import RegisterForm from "@/app/_components/ui/Register/RegisterForm";
+import LogoutButton from "@/app/_components/ui/Logout/LogoutButton";
+import UserDetailsClientComponent from "@/app/_components/ui/Example/UserAuthDetails/ClientComponent";
+import UserDetailsServerComponent from "@/app/_components/ui/Example/UserAuthDetails/ServerComponent";
+import SkeletonUserDetail from "@/app/_components/ui/Example/Skeleton/UserDetails";
+import { Suspense } from "react";
+// import { auth } from "@/server/auth";
+
 export default function Dashboard() {
   return (
     <main>
-      <div>Dashboard</div>
+      <UserDetailsClientComponent />
+      <Suspense fallback={<SkeletonUserDetail />}>
+        <UserDetailsServerComponent />
+      </Suspense>
+      <RegisterForm />
+      <LogoutButton />
     </main>
   );
 }
