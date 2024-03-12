93fa38ff7739b622df02d67d801426abe9a3674d
Yottrakul Suwannasri <zxc734@gmail.com>
2024-03-12 16:15:04 +0700

    fix(nextauth): add noStore() in UserManagement

--- src/app/_components/ui/user/UserManagement.tsx
+++ src/app/_components/ui/user/UserManagement.tsx
@@ -1,7 +1,9 @@
 import { getUserAll } from "@/app/_actions/data/user";
 import UserManagementTable from "@/app/_components/ui/user/table/UserManagementTable";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default async function UserManagement() {
+  noStore();
   const users = await getUserAll();
 
   return (
