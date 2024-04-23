bf29c5148a823c85cf5723def041ed16d67eee1c
Yottrakul Suwannasri <zxc734@gmail.com>
2024-04-23 15:28:01 +0700

    fix(bar): bar has returned

--- src/app/_components/ui/DashboardOverview/BarChart.tsx
+++ src/app/_components/ui/DashboardOverview/BarChart.tsx
@@ -1,7 +1,7 @@
 "use client";
-// import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
-// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const options = {
   responsive: true,
