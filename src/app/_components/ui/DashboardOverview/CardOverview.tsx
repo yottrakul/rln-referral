ab1b781a62113e082625658ae9c728a23be21ff6
Thanapat Rattanamontree <thanapat16849@gmail.com>
2024-05-04 13:38:49 +0700

    fix(cardDashboard)

--- src/app/_components/ui/DashboardOverview/CardOverview.tsx
+++ src/app/_components/ui/DashboardOverview/CardOverview.tsx
@@ -23,19 +23,18 @@ export default function CardOverview({ title, body, date, bgcolor }: CardOvervie
 
 
   return (
-    <>
-      <Card size="md" sx={cardStyle} >
+    <Box w={"full"}>
+      <Card size="md">
         <CardHeader bg={bgcolor}>
           <Flex>
             <PiNumberCircleOneLight size={40} />
-            <Text className={style.customHeading} ml={1}>
+            <Text whiteSpace={"nowrap"} overflow={"hidden"} textOverflow={"ellipsis"} className={style.customHeading} ml={1}>
               {title}
             </Text>
           </Flex>
         </CardHeader>
         <CardBody>
-          {/* <Text className={style.custom1}>{cardInformation} รายการ</Text> */}
-          <Text className={style.custom1}><CardProcess status={body}/> รายการ</Text>
+          <Text className={style.custom1}>{body} รายการ</Text>
         </CardBody>
         <Divider></Divider>
         <CardFooter>
@@ -44,6 +43,6 @@ export default function CardOverview({ title, body, date, bgcolor }: CardOvervie
           <Text>{date}</Text>
         </CardFooter>
       </Card>
-    </>
+    </Box>
   );
 }
